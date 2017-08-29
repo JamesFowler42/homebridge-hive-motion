8b2946025514cac57be2d0b7aa00a15dbb7bac00
James Fowler <keith@difficult.me.uk>
2017-08-29 16:15:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var Service, Characteristic;
 var request = require("request");
-var fs = require("fs");
-var homebridgetmp = "/var2/homebridge/";
 module.exports = function(homebridge){
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("homebridge-hive", "HiveThermostat", HiveThermostat);
+  homebridge.registerAccessory("homebridge-hive-motion", "HiveMotionSensor", HiveMotionSensor);
 };
-function HiveThermostat(log, config) {
+
+function HiveMotionSensor(log, config) {
 	this.log = log;
 	this.name = config.name;
-	this.thermostatService = new Service.Thermostat(this.name);
+	this.sensorService = new Service.MotionSensor();
 	this.informationService = new Service.AccessoryInformation();
 	this.username = config.username;
 	this.password = config.password;
@@ -30,7 +29,7 @@ function HiveThermostat(log, config) {
 	this.debug = config.hasOwnProperty('debug') ? config.debug : false;
 }
 
-HiveThermostat.prototype = {
+HiveMotionSensor.prototype = {
 	
 	identify: function(callback) {
 		callback(null);
@@ -104,35 +103,16 @@ HiveThermostat.prototype = {
 		var successHandler = function(body){
 			/* Parse the response */
 			for ( var i = 0; i < body.nodes.length; i++ ) {
-				if ( body.nodes[i].nodeType == "http:\/\/alertme.com\/schema\/json\/node.class.thermostat.json#" && body.nodes[i].attributes.temperature && ( !this.id || body.nodes[i].id == this.id ) ) {
-					this.cachedMainData = body.nodes[i];
-					if ( showIds ) {
-						this.log("Found thermostat " + body.nodes[i].id + ". Current temperature is " + body.nodes[i].attributes.temperature.reportedValue + ", set to " + body.nodes[i].attributes.targetHeatTemperature.reportedValue );
-					}
-				} else if ( body.nodes[i].nodeType == "http:\/\/alertme.com\/schema\/json\/node.class.motion.sensor.json#") {
+				if ( body.nodes[i].nodeType == "http:\/\/alertme.com\/schema\/json\/node.class.motion.sensor.json#") {
 					var sensorName = body.nodes[i].name;
+					if (sensorName !== this.name ) {
+						this.log("Ignoring " + sensorName);
+					}
 					var sensorMotionStarted = body.nodes[i].attributes.motionStarted.displayValue;
 					var sensorMotionEnded = body.nodes[i].attributes.motionEnded.displayValue;
 					var sensorInMotion = body.nodes[i].attributes.inMotion.displayValue;
-					var sensorFileName = homebridgetmp + sensorName.replace(/\s+/g, '').toLowerCase() + ".ping";
-					this.log("Found motion sensor " + body.nodes[i].id + ", name:" + sensorName + ", motion=" + sensorInMotion + ", from=" + sensorMotionStarted + ", to=" + sensorMotionEnded + ", file=" + sensorFileName);
-					if (sensorInMotion == "true") {
-						fs.writeFile(sensorFileName, sensorMotionStarted + " to " + sensorMotionEnded, (err) => {
-    						if (err) {
-								this.log("Failed to create file " + sensorFileName);
-							} else {
-								this.log("Created file " + sensorFileName);
-							}
-                        }); 
-					} else {
-						fs.unlink(sensorFileName, (err) => {
-    						if (err) {
-								this.log("Unable to delete file " + sensorFileName);
-							} else {
-								this.log("Deleted file " + sensorFileName);
-							}
-                        });
-					}
+					this.cachedMainData = body.nodes[i];
+					this.log("Found motion sensor " + body.nodes[i].id + ", name:" + sensorName + ", motion=" + sensorInMotion + ", from=" + sensorMotionStarted + ", to=" + sensorMotionEnded);
 				}
 			}
 			this.cachedDataTime = Date.now()
@@ -190,235 +170,21 @@ HiveThermostat.prototype = {
 	/* -------------------- */
 
 	getServices: function() {
-		
-		/* -------------------- */
-		/* !Thermostat			*/
-		/* -------------------- */
 
 		/**
-		 * Get Current Temperature (Read Only)
+		 * Sensor status
 		 */
-		this.thermostatService.getCharacteristic(Characteristic.CurrentTemperature)
+		this.sensorService.getCharacteristic(Characteristic.MotionDetected)
 			.on('get', function(callback) {
 				this.getMainData(function(error,data){
-					this.log( "Current temperature is " + data.attributes.temperature.reportedValue );
-					callback( error, data.attributes.temperature.reportedValue );
+					var sensorInMotion = data.attributes.inMotion.displayValue;
+					this.log( "In motion " + sensorInMotion );
+					callback( error, sensorInMotion );
 				}.bind(this));
 			}.bind(this))
 		;
-		
-		/**
-		 * Get Current Heating/Cooling State (Read Only - i.e. what the system is doing right now)
-		 *
-		 * Characteristic.CurrentHeatingCoolingState.OFF = Not Heating (either because it is turned off or has reached the desired temprature)
-		 * Characteristic.CurrentHeatingCoolingState.HEAT = Heating
-		 *
-		 * Cannot be Auto because we want to know what it's doing *right now*
-		 *
-		 */
-		this.thermostatService.getCharacteristic(Characteristic.CurrentHeatingCoolingState)
-			.on('get', function(callback) {
-				this.getMainData(function(error,data){
-					
-					if ( data.attributes.stateHeatingRelay.reportedValue == 'OFF' ) {
-						var currentHeatingCoolingState = Characteristic.CurrentHeatingCoolingState.OFF;
-					} else {
-						var currentHeatingCoolingState = Characteristic.CurrentHeatingCoolingState.HEAT;
-					}
-					
-					this.log( "Current state is " + currentHeatingCoolingState );
-					callback( error, currentHeatingCoolingState );
-					
-				}.bind(this));
-			}.bind(this))
-		;
-		
-		/**
-		 * Target Heating/Cooling State (Can be read and set - is what the system is doing now or we have told it we want to do)
-		 *
-		 * Characteristic.TargetHeatingCoolingState.OFF = Off / Frost Protect
-		 * Characteristic.TargetHeatingCoolingState.HEAT = Heat when below target temperature
-		 *
-		 */
-		this.thermostatService.getCharacteristic(Characteristic.TargetHeatingCoolingState)
-			
-			/**
-			 * Get
-			 */
-			.on('get', function(callback) {
-				this.getMainData(function(error,data){
-					
-					if ( data.attributes.activeHeatCoolMode.reportedValue == 'OFF' || data.attributes.targetHeatTemperature.reportedValue == 1 ) {
-						var targetHeatingCoolingState = Characteristic.TargetHeatingCoolingState.OFF;
-					} else {
-						var targetHeatingCoolingState = Characteristic.TargetHeatingCoolingState.HEAT;
-					}
-					
-					this.log( "Target state is " + targetHeatingCoolingState );
-					callback( error, targetHeatingCoolingState );
-					
-				}.bind(this));
-			}.bind(this))
-						
-			/**
-			 * Set
-			 */
-			.on('set', function(value, callback) {
-								
-				/* We need to know our current state so we know what we're changing... */
-				this.getMainData(function(error,data){
-									
-					/* Work out what we need to set */			
-					var stateForHive = null;
-					var stateForHomebridge = null;
-					switch ( value ) {
-						case Characteristic.TargetHeatingCoolingState.OFF:
-						case Characteristic.TargetHeatingCoolingState.COOL:
-							stateForHive = 'OFF';
-							stateForHomebridge = Characteristic.TargetHeatingCoolingState.OFF;
-							break;
-						case Characteristic.TargetHeatingCoolingState.HEAT:
-						case Characteristic.TargetHeatingCoolingState.AUTO:
-							stateForHive = 'MANUAL';
-							stateForHomebridge = Characteristic.TargetHeatingCoolingState.HEAT;
-							break;
-					}
-									
-					/* Set it */
-					request.put({
-						url: "https://api-prod.bgchprod.info:443/omnia/nodes/" + data.id,
-						headers: {
-							'Content-Type': 'application/vnd.alertme.zoo-6.1+json',
-							'Accept': 'application/vnd.alertme.zoo-6.1+json',
-							'X-Omnia-Client': 'Hive Web Dashboard',
-							'X-Omnia-Access-Token': this.apiKey
-						},
-						body: JSON.stringify({
-							"nodes": [{
-						        "attributes": {
-						            "activeHeatCoolMode": {
-						                "targetValue": stateForHive
-						            }
-						        }
-						    }]
-						})
-					},
-									
-					/* Once we have it... */
-					function(error, response, body) {	
-						this.log( "Set target state to " + stateForHomebridge );
-						callback(null);
-					}.bind(this));
-					
-				}.bind(this))
-				
-				
-			}.bind(this))
-			
-		;
-		
-		/**
-		 * Target Temperature (Can be read and set)
-		 */
-		this.thermostatService.getCharacteristic(Characteristic.TargetTemperature)
-			
-			/**
-			 * Get
-			 */
-			.on('get', function(callback) {
-				this.getMainData(function(error,data){
-					var targetTemperature = data.attributes.targetHeatTemperature.reportedValue;
-					if ( targetTemperature == 1 ) {
-						targetTemperature = data.attributes.frostProtectTemperature.reportedValue;
-					}
-					this.log( "Target temperature is " + targetTemperature );
-					callback(error,targetTemperature);
-				}.bind(this));
-			}.bind(this))
-			
-			/**
-			 * Min/Max Values for setting
-			 */
-			.setProps({
-				minValue: 5.0,
-				maxValue: 32.0,
-				minStep: 0.5
-			})
-			
-			/**
-			 * Set
-			 */
-			.on('set', function(value, callback) {
-				
-				/* We need to know our current state so we know what we're changing... */
-				this.getMainData(function(error,data){
-					
-					/* Set it */			
-					request.put({
-						url: "https://api-prod.bgchprod.info:443/omnia/nodes/" + data.id,
-						headers: {
-							'Content-Type': 'application/vnd.alertme.zoo-6.1+json',
-							'Accept': 'application/vnd.alertme.zoo-6.1+json',
-							'X-Omnia-Client': 'Hive Web Dashboard',
-							'X-Omnia-Access-Token': this.apiKey
-						},
-						body: JSON.stringify({
-							"nodes": [{
-						        "attributes": {
-						            "targetHeatTemperature": {
-						                "targetValue": value
-						            }
-						        }
-						    }]
-						})
-					},
-					
-					/* Once we have it... */
-					function(error, response, body) {
-						this.log( "Set target temperature to " + value );
-						
-						/* Does this mean the thermostat will turn on/off? */
-						if ( value > data.attributes.temperature.reportedValue && data.attributes.stateHeatingRelay.reportedValue == 'OFF' ) {
-							this.thermostatService.getCharacteristic(Characteristic.CurrentHeatingCoolingState)
-								.setValue( Characteristic.CurrentHeatingCoolingState.HEAT, function(){
-									callback(null);
-								}.bind(this) );
-						} else if ( value < data.attributes.temperature.reportedValue && data.attributes.stateHeatingRelay.reportedValue == 'HEAT' ) {
-							this.thermostatService.getCharacteristic(Characteristic.CurrentHeatingCoolingState)
-								.setValue( Characteristic.CurrentHeatingCoolingState.OFF, function(){
-									callback(null);
-								}.bind(this) );
-						} else {						
-							callback(null);
-						}
-					}.bind(this));
-					
-				}.bind(this));
-				
-			}.bind(this))
-			
-		;
-		
-		/**
-		 * Hardware display units (in theory can be read and set but we don't actually allow this)
-		 */
-		this.thermostatService.getCharacteristic(Characteristic.TemperatureDisplayUnits)
-			
-			/**
-			 * Get
-			 */
-			.on('get', function(callback) {
-				callback( null, Characteristic.TemperatureDisplayUnits.CELSIUS );
-			}.bind(this))
 
-			/**
-			 * Set
-			 */
-			.on('set', function(value, callback) {
-				callback("Cannot change display units");
-			}.bind(this))
-		;		
-				
+		
 		/* --------------------- */
 		/* !AccessoryInformation */
 		/* --------------------- */
@@ -428,9 +194,6 @@ HiveThermostat.prototype = {
 			.setCharacteristic(Characteristic.Model, "Hive Active Heating")
 			.setCharacteristic(Characteristic.SerialNumber, " ");
 		
-		
-		
-		
-		return [this.thermostatService,this.informationService];
+		return [this.sensorService,this.informationService];
 	}
 };
